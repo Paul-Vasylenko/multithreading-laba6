70715024e9dd6fbccd2d61e10f47646cae7dbdcc
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-05-25 17:00:27 +0300

    Use non blocking methods

--- src/Main.java
+++ src/Main.java
@@ -43,18 +43,22 @@ public class Main {
                 rows[0] = (dest <= extra) ? rowsPerThread + 1 : rowsPerThread;
                 System.out.println("Sending " + rows[0] + " rows to task " + dest + " offset="+offset[0]);
 
-                MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(a, offset[0], rows[0], MPI.OBJECT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, dest, FROM_MASTER);
+                MPI.COMM_WORLD.Isend(offset, 0, 1, MPI.INT, dest, 0);
+                MPI.COMM_WORLD.Isend(rows, 0, 1, MPI.INT, dest, 1);
+                MPI.COMM_WORLD.Isend(a, offset[0], rows[0], MPI.OBJECT, dest, 2);
+                MPI.COMM_WORLD.Isend(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, dest, 3);
 
                 offset[0] = offset[0] + rows[0];
             }
             System.out.println("****Results: ");
             for(int source = 1; source <= numWorkers; source++) {
-                MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, source, TO_MASTER);
-                MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, source, TO_MASTER);
-                MPI.COMM_WORLD.Recv(c, offset[0], rows[0], MPI.OBJECT, source, TO_MASTER);
+                var offsetRequest = MPI.COMM_WORLD.Irecv(offset, 0, 1, MPI.INT, source, 4);
+                var rowsRequest = MPI.COMM_WORLD.Irecv(rows, 0, 1, MPI.INT, source, 5);
+                offsetRequest.Wait();
+                rowsRequest.Wait();
+                var matrixRequest = MPI.COMM_WORLD.Irecv(c, offset[0], rows[0], MPI.OBJECT, source, 6);
+                matrixRequest.Wait();
+                System.out.println("Data received by master process");
             }
 
             for(int i = 0; i < NUMBER_ROWS_A; i++) {
@@ -64,13 +68,19 @@ public class Main {
                 System.out.print('\n');
             }
         } else {
-            MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(a, 0, rows[0], MPI.OBJECT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, MASTER, FROM_MASTER);
+            var offsetRequest = MPI.COMM_WORLD.Irecv(offset, 0, 1, MPI.INT, MASTER, 0);
+            var rowsRequest = MPI.COMM_WORLD.Irecv(rows, 0, 1, MPI.INT, MASTER, 1);
+            // This data is needed first to get matrices
+            offsetRequest.Wait();
+            rowsRequest.Wait();
+            MPI.COMM_WORLD.Isend(offset, 0, 1, MPI.INT, MASTER, 4);
+            MPI.COMM_WORLD.Isend(rows, 0, 1, MPI.INT, MASTER, 5);
 
+            var aRequest = MPI.COMM_WORLD.Irecv(a, 0, rows[0], MPI.OBJECT, MASTER, 2);
+            var bRequest = MPI.COMM_WORLD.Irecv(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, MASTER, 3);
+            aRequest.Wait();
+            bRequest.Wait();
             System.out.println("Data was received by " + rank + " process");
-
             for (int k = 0; k < NUMBER_COLUMNS_B; k++) {
                 for (int i = 0; i < rows[0]; i++) {
                     for (int j = 0; j < NUMBER_COLUMNS_A; j++) {
@@ -79,9 +89,8 @@ public class Main {
                 }
             }
 
-            MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, MASTER, TO_MASTER);
-            MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, MASTER, TO_MASTER);
-            MPI.COMM_WORLD.Send(c, 0, rows[0], MPI.OBJECT, MASTER, TO_MASTER);
+
+            MPI.COMM_WORLD.Isend(c, 0, rows[0], MPI.OBJECT, MASTER, 6);
         }
 
         MPI.Finalize();
