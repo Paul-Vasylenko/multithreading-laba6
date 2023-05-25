d22c52bd849768e00076bf9b70f27797a054f875
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-05-25 21:04:59 +0300

    Collective methods

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import mpi.MPI;
 
 public class Main {
-    private static final int NUMBER_ROWS_A = 100;
-    private static final int NUMBER_COLUMNS_A = 100;
-    private static final int NUMBER_COLUMNS_B = 100;
+    private static final int NUMBER_ROWS_A = 2000;
+    private static final int NUMBER_COLUMNS_A = 2000;
+    private static final int NUMBER_COLUMNS_B = 2000;
     private static final int MASTER = 0;
-    private static final int FROM_MASTER = 1;
-    private static final int TO_MASTER = 2;
     public static void main(String[] args) {
         double[][] a = new double[NUMBER_ROWS_A][NUMBER_COLUMNS_A];
         double[][] b = new double[NUMBER_COLUMNS_A][NUMBER_COLUMNS_B];
@@ -19,73 +17,69 @@ public class Main {
             System.out.println("Need at least two MPI tasks. Quitting...\n");
             MPI.COMM_WORLD.Abort(1);
         }
-        int numWorkers = numTasks - 1;
-        int[] offset = {0};
-        int[] rows = {0};
+        long start = 0;
         if(rank == MASTER) {
-//            System.out.println("MPI started with " + numTasks + " tasks");
-
-            for(int i = 0; i < NUMBER_ROWS_A; i++) {
+            for (int i = 0; i < NUMBER_ROWS_A; i++) {
                 for (int j = 0; j < NUMBER_COLUMNS_A; j++) {
-                    a[i][j] = 10;
+                    a[i][j] = i*2 + j;
                 }
             }
-            for(int i = 0; i < NUMBER_COLUMNS_A; i++) {
+            for (int i = 0; i < NUMBER_COLUMNS_A; i++) {
                 for (int j = 0; j < NUMBER_COLUMNS_B; j++) {
-                    b[i][j] = 10;
+                    b[i][j] = j*2+i;
                 }
             }
+            start = System.currentTimeMillis();
+        }
+        int rowsPerThread = NUMBER_ROWS_A / numTasks;
+        int extra = NUMBER_ROWS_A % numTasks;
 
-            int rowsPerThread = NUMBER_ROWS_A / numWorkers;
-            int extra = NUMBER_ROWS_A % numWorkers;
-            var start = System.currentTimeMillis();
-            for(int dest = 1; dest <= numWorkers; dest++) {
-                rows[0] = (dest <= extra) ? rowsPerThread + 1 : rowsPerThread;
-//                System.out.println("Sending " + rows[0] + " rows to task " + dest + " offset="+offset[0]);
+        int[] rowsCounts = new int[numTasks];
+        int[] offsetCounts = new int[numTasks];
+        for(int i = 0; i < numTasks; i++) {
+            rowsCounts[i] = i < extra ? rowsPerThread + 1 : rowsPerThread;
+            offsetCounts[i] = i == MASTER ? 0 : offsetCounts[i-1] + rowsCounts[i-1];
+        }
 
-                MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(a, offset[0], rows[0], MPI.OBJECT, dest, FROM_MASTER);
-                MPI.COMM_WORLD.Send(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, dest, FROM_MASTER);
+        int rowsForThisTask = rowsCounts[rank];
+        double[][] aRows = new double[rowsForThisTask][NUMBER_COLUMNS_A];
+        // Send rows of matrix A
+        MPI.COMM_WORLD.Scatterv(
+                a, 0, rowsCounts, offsetCounts,MPI.OBJECT,
+                aRows, 0, rowsForThisTask, MPI.OBJECT,
+                MASTER
+        );
+        // Send all matrix B
+        MPI.COMM_WORLD.Bcast(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, MASTER);
 
-                offset[0] = offset[0] + rows[0];
+        double[][] cRows = new double[rowsForThisTask][NUMBER_COLUMNS_B];
+        for (int k = 0; k < NUMBER_COLUMNS_B; k++) {
+            for (int i = 0; i < rowsForThisTask; i++) {
+                for (int j = 0; j < NUMBER_COLUMNS_A; j++) {
+                    cRows[i][k] += aRows[i][j] * b[j][k];
+                }
             }
-//            System.out.println("****Results: ");
-            for(int source = 1; source <= numWorkers; source++) {
-                MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, source, TO_MASTER);
-                MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, source, TO_MASTER);
-                MPI.COMM_WORLD.Recv(c, offset[0], rows[0], MPI.OBJECT, source, TO_MASTER);
-            }
-            var end = System.currentTimeMillis();
-            var dur = end - start;
-            System.out.println("time: " + dur);
+        }
+//        System.out.println(rank + " : " + Arrays.toString(cRows[0]));
+
+        MPI.COMM_WORLD.Gatherv(
+                cRows, 0, rowsForThisTask, MPI.OBJECT,
+                c, 0, rowsCounts, offsetCounts, MPI.OBJECT,
+                MASTER
+        );
+
+        if (rank == MASTER) {
+            var endTime = System.currentTimeMillis();
+            var dur = endTime - start;
+
 //            for(int i = 0; i < NUMBER_ROWS_A; i++) {
 //                for (int j = 0; j < NUMBER_COLUMNS_B; j++) {
 //                    System.out.print(c[i][j] +" ");
 //                }
 //                System.out.print('\n');
 //            }
-        } else {
-            MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(a, 0, rows[0], MPI.OBJECT, MASTER, FROM_MASTER);
-            MPI.COMM_WORLD.Recv(b, 0, NUMBER_COLUMNS_A, MPI.OBJECT, MASTER, FROM_MASTER);
-
-//            System.out.println("Data was received by " + rank + " process");
-
-            for (int k = 0; k < NUMBER_COLUMNS_B; k++) {
-                for (int i = 0; i < rows[0]; i++) {
-                    for (int j = 0; j < NUMBER_COLUMNS_A; j++) {
-                        c[i][k] += a[i][j] * b[j][k];
-                    }
-                }
-            }
-
-            MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, MASTER, TO_MASTER);
-            MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, MASTER, TO_MASTER);
-            MPI.COMM_WORLD.Send(c, 0, rows[0], MPI.OBJECT, MASTER, TO_MASTER);
+            System.out.println("time: " + dur);
         }
-
         MPI.Finalize();
     }
 }
